2bad5c8e18c780e36ecd8326023279792fc21815
nicola <niquola@gmail.com>
2013-04-21 01:56:03 +0400

    small progress

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,4 @@
 class Issue < ActiveRecord::Base
   belongs_to :project
+  validate :status, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 AngularOnRails::Application.routes.draw do
-  resources :issues
-
-
   match ':controller(/:action).ng', format: 'ng'
-  resources :feeds do
-    collection { get :preview }
-    resources :entries
-  end
-
   resources :projects do
     resources :issues
   end
+  resources :issues
   root :to => 'feeds#index'
 end
